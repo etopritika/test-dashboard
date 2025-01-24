4da54b8c79b9f73737a9d0801a8602440aa8236d
etopritika <dimaprytyka@gmail.com>
2025-01-24 16:04:43 +0200

    Updated types for charts

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -3,3 +3,15 @@ export type User = {
   email: string;
   password: string;
 };
+
+export type MonthlyData = {
+  month: string;
+  losses: number;
+};
+
+export type YearlyData = {
+  year: number;
+  months: MonthlyData[];
+};
+
+export type ProcessedData = { month: string; value: number }[];
