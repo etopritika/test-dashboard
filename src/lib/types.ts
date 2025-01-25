bf622701d34c8f91a8e29e8c13364b21c0dcf940
etopritika <dimaprytyka@gmail.com>
2025-01-25 19:15:11 +0200

    Updated types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -15,3 +15,15 @@ export type YearlyData = {
 };
 
 export type ProcessedData = { month: string; value: number }[];
+
+export type Losses = {
+  category: string;
+  count: number;
+};
+
+export type DayData = {
+  date: string;
+  losses: Losses[];
+};
+
+export type TableData = DayData[];
