484433c3eca53daaa4450c6e9bfe1c465f8a7f14
etopritika <dimaprytyka@gmail.com>
2025-01-25 17:51:08 +0200

    Updated styles in login page, added min-h-screen

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,7 +4,7 @@ import AuthWrapper from "@/components/Auth-Wrapper";
 export default function Login() {
   return (
     <AuthWrapper redirectIfAuthenticated={true}>
-      <section className="h-full flex items-center justify-center">
+      <section className="min-h-screen flex items-center justify-center">
         <h1 className="sr-only">Authorization Page</h1>
         <AuthForm />
       </section>
